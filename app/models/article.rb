90553aa04ff838cafbf46189506d6a16bb750d6a
geraldy1st <geraldy.leondas@gmail.com>
2018-06-24 00:17:07 +0200

    Refactoring

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,7 @@ class Article < ApplicationRecord
                     length: { minimum: 5 }
   validates :text, presence: true,
                     length: { minimum: 5 }
+  has_many :comments
+  validates :title, presence: true,
+                    length: { minimum: 5 }
 end
