0a60c838710dba617c0d3676a41b4421cf8cc6d7
geraldy1st <geraldy.leondas@gmail.com>
2018-07-03 15:57:18 +0200

    maj 16h00

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,7 +1,15 @@
 $(document).on("turbolinks:load", function(){
+
   $(".devise-main-edit").hide();
   $(".cancel-account").click(function() {
     $(".devise-main-edit").toggle(200);
   });
-
+  
 });
+// var nombre = Number(prompt("Whats your lucky number :"));
+// if (nombre > 0) {
+//     alert("the number " + nombre + " will give you luck");
+// }
+// else {
+//   alert("aiieeee")
+// }
